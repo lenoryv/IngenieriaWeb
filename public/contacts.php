d6fe23bbf8c8ea0ff97d0df9887629c18c3d1d24
leor0104 <luisortiz141999@gmail.com>
2020-05-18 23:49:25 -0500

    MySql

---
+++ public/contacts.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>TALLER 3</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/style_page.css">
+    <script src="https://kit.fontawesome.com/4feb140172.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php include '../components/nav.php'; ?>
+    <main class="content_page">
+        <div class="content">
+            <h2 class="title">CONTACTOS</h2>
+        </div>
+        <p>Más información proximamente ...</p>
+    </main>
+    <?php include '../components/footer.php'; ?>
+</body>
+
+</html>
